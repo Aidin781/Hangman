ec1539b21feea5df23bf6ec9830addf71489a737
Aidin781 <anazekov@gmail.com>
2017-04-10 20:33:15 +0600

    Create initial game logic

--- app/src/main/java/com/example/hp/hangman/MainActivity.java
+++ app/src/main/java/com/example/hp/hangman/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hp.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView lettersView;
     private EditText guessView;
     private Button submitButton;
+    private hangManController hangManController;
+    private hangManView hangManView;
+    private hangManModel hangManModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                update();
+            }
+        });
+
+        hangManController.setWord("zagaka");
+
+    }
+
+    private void update() {
+        hangManController.acceptGuess(guessView.getText().toString());
+        hangManView.update(hangManModel);
+        gallowsView.setText(hangManView.getGallow());
+        wordView.setText(hangManView.getWord());
+        lettersView.setText(hangManView.getLetters());
+
     }
 
     private void init() {
@@ -28,6 +51,10 @@ public class MainActivity extends AppCompatActivity {
         lettersView = (TextView)findViewById(R.id.textViewLetters);
         guessView = (EditText)findViewById(R.id.editTextGuess);
         submitButton = (Button)findViewById(R.id.button);
+
+        hangManModel = new HangManModel();
+        hangMandController = new HangManController(hangManModel);
+        hangManView = new HangManView;
 
     }
 }
