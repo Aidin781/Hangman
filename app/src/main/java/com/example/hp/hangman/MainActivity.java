52079dcf89d7fa0fbb407139c43500cb77ab0418
Aidin781 <anazekov@gmail.com>
2017-04-10 19:52:43 +0600

    First commit

---
+++ app/src/main/java/com/example/hp/hangman/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.hp.hangman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView gallowsView;
+    private TextView wordView;
+    private TextView lettersView;
+    private EditText guessView;
+    private Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        gallowsView = (TextView)findViewById(R.id.textViewGallows);
+        wordView = (TextView)findViewById(R.id.textViewWords);
+        lettersView = (TextView)findViewById(R.id.textViewLetters);
+        guessView = (EditText)findViewById(R.id.editTextGuess);
+        submitButton = (Button)findViewById(R.id.button);
+
+    }
+}
